d5ffbf305fd4db650b4423819e35e76226b44e34
StHichy <stormhichy@gmail.com>
2026-08-27 11:20:38 -0300

    0.0.2 - Adição de um contador de pizzas ao pedido

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+let quantidadePizzas = 0;
+
 function adicionarPedido(nomePizza) {
 
+    quantidadePizzas++;
+
     const mensagem = document.getElementById("mensagem");
+
+    const contador = document.getElementById("contador");
 
     mensagem.innerHTML =
         "A pizza <strong>" +
         nomePizza +
         "</strong> foi adicionada ao seu pedido! 🍕";
+
+    contador.innerHTML =
+        "Total de pizzas: " + quantidadePizzas;
 
 }
